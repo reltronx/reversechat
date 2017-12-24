378f95f0570659bee9bb9af5f7eedbd0249d300a
Abhinava <reltronx@gmail.com>
2017-12-24 18:50:32 +0530

    Emit newMEssage on createMessage event occurence

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+var socket = io();
+
+socket.on('connect', () => {
+    console.log('connected to server');
+
+});
+
+socket.on('disconnect' , () =>{
+    console.log('Client Disconnected ');
+});
+
+socket.on('newMessage', function(message){
+    console.log('New message Arrived',message);
+});
